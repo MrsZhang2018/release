06ffcd822704ca2b6b32917fa6cc8c611ffa2199
Eric <eric.lob88088@gmail.com>
2018-07-10 09:44:50 +0800

    1

--- libraryFanwe/src/main/java/com/fanwe/library/customview/SDSendValidateButton.java
+++ libraryFanwe/src/main/java/com/fanwe/library/customview/SDSendValidateButton.java
@@ -12,238 +12,197 @@ import com.fanwe.library.utils.SDTimer;
 import com.fanwe.library.utils.SDTimer.SDTimerListener;
 import com.fanwe.library.utils.SDViewUtil;
 
-public class SDSendValidateButton extends Button
-{
+public class SDSendValidateButton extends Button {
 
-	private static final int DISABLE_TIME = 60;
+    private static final int DISABLE_TIME = 60;
 
-	private SDTimer mTimer = new SDTimer();
+    private SDTimer mTimer = new SDTimer();
 
-	private int mDisableTime = DISABLE_TIME; // 倒计时时间，默认60秒
-	private int mTextColorEnable = Color.WHITE;
-	private int mTextColorDisable = Color.WHITE;
+    private int mDisableTime = DISABLE_TIME; // 倒计时时间，默认60秒
+    private int mTextColorEnable = Color.WHITE;
+    private int mTextColorDisable = Color.WHITE;
 
-	private String mTextEnable = "获取验证码";
-	private String mTextDisable = "秒后重发验证码";
+    private String mTextEnable = "获取验证码";
+    private String mTextDisable = "秒后重发验证码";
 
-	private int mBackgroundEnableResId = 0;
-	private int mBackgroundDisableResId = 0;
+    private int mBackgroundEnableResId = 0;
+    private int mBackgroundDisableResId = 0;
 
-	private int mDisableTimeTemp = 0;
+    private int mDisableTimeTemp = 0;
 
-	private boolean mClickAble = true;
+    private boolean mClickAble = true;
 
-	private SDSendValidateButtonListener mListener = null;
+    private SDSendValidateButtonListener mListener = null;
 
-	// ----------------get set
+    // ----------------get set
 
-	public int getmDisableTime()
-	{
-		return mDisableTime;
-	}
+    public int getmDisableTime() {
+        return mDisableTime;
+    }
 
-	public void setmDisableTime(int mDisableTime)
-	{
-		this.mDisableTime = mDisableTime;
-	}
+    public void setmDisableTime(int mDisableTime) {
+        this.mDisableTime = mDisableTime;
+    }
 
-	public int getCurrentDisableTime()
-	{
-		return mDisableTimeTemp;
-	}
+    public int getCurrentDisableTime() {
+        return mDisableTimeTemp;
+    }
 
-	public int getmTextColorEnable()
-	{
-		return mTextColorEnable;
-	}
+    public int getmTextColorEnable() {
+        return mTextColorEnable;
+    }
 
-	public void setmTextColorEnable(int mTextColorEnable)
-	{
-		this.mTextColorEnable = mTextColorEnable;
-	}
+    public void setmTextColorEnable(int mTextColorEnable) {
+        this.mTextColorEnable = mTextColorEnable;
+    }
 
-	public int getmTextColorDisable()
-	{
-		return mTextColorDisable;
-	}
+    public int getmTextColorDisable() {
+        return mTextColorDisable;
+    }
 
-	public void setmTextColorDisable(int mTextColorDisable)
-	{
-		this.mTextColorDisable = mTextColorDisable;
-	}
+    public void setmTextColorDisable(int mTextColorDisable) {
+        this.mTextColorDisable = mTextColorDisable;
+    }
 
-	public String getmTextEnable()
-	{
-		return mTextEnable;
-	}
+    public String getmTextEnable() {
+        return mTextEnable;
+    }
 
-	public void setmTextEnable(String mTextEnable)
-	{
-		this.mTextEnable = mTextEnable;
-	}
+    public void setmTextEnable(String mTextEnable) {
+        this.mTextEnable = mTextEnable;
+    }
 
-	public String getmTextDisable()
-	{
-		return mTextDisable;
-	}
+    public String getmTextDisable() {
+        return mTextDisable;
+    }
 
-	public void setmTextDisable(String mTextDisable)
-	{
-		this.mTextDisable = mTextDisable;
-	}
+    public void setmTextDisable(String mTextDisable) {
+        this.mTextDisable = mTextDisable;
+    }
 
-	public int getmBackgroundEnableResId()
-	{
-		return mBackgroundEnableResId;
-	}
+    public int getmBackgroundEnableResId() {
+        return mBackgroundEnableResId;
+    }
 
-	public void setmBackgroundEnableResId(int mBackgroundEnableResId)
-	{
-		this.mBackgroundEnableResId = mBackgroundEnableResId;
-	}
+    public void setmBackgroundEnableResId(int mBackgroundEnableResId) {
+        this.mBackgroundEnableResId = mBackgroundEnableResId;
+    }
 
-	public int getmBackgroundDisableResId()
-	{
-		return mBackgroundDisableResId;
-	}
+    public int getmBackgroundDisableResId() {
+        return mBackgroundDisableResId;
+    }
 
-	public void setmBackgroundDisableResId(int mBackgroundDisableResId)
-	{
-		this.mBackgroundDisableResId = mBackgroundDisableResId;
-	}
+    public void setmBackgroundDisableResId(int mBackgroundDisableResId) {
+        this.mBackgroundDisableResId = mBackgroundDisableResId;
+    }
 
-	public SDSendValidateButtonListener getmListener()
-	{
-		return mListener;
-	}
+    public SDSendValidateButtonListener getmListener() {
+        return mListener;
+    }
 
-	public void setmListener(SDSendValidateButtonListener mListener)
-	{
-		this.mListener = mListener;
-	}
+    public void setmListener(SDSendValidateButtonListener mListener) {
+        this.mListener = mListener;
+    }
 
-	public SDSendValidateButton(Context context)
-	{
-		this(context, null);
-	}
+    public SDSendValidateButton(Context context) {
+        this(context, null);
+    }
 
-	public SDSendValidateButton(Context context, AttributeSet attrs)
-	{
-		super(context, attrs);
-		initView();
-	}
+    public SDSendValidateButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
 
-	private void initView()
-	{
-		mTextColorEnable = SDLibrary.getInstance().getConfig().getMainColor();
-		mTextColorDisable = SDLibrary.getInstance().getConfig().getMainColor();
+    private void initView() {
+        mTextColorEnable = SDLibrary.getInstance().getConfig().getMainColor();
+        mTextColorDisable = SDLibrary.getInstance().getConfig().getMainColor();
 
-		GradientDrawable gradientDrawable = new GradientDrawable();
-		gradientDrawable.setColor(Color.TRANSPARENT);
-		gradientDrawable.setStroke(SDViewUtil.dp2px(1), mTextColorEnable);
-		gradientDrawable.setCornerRadius(SDViewUtil.dp2px(5));
+        GradientDrawable gradientDrawable = new GradientDrawable();
+        gradientDrawable.setColor(Color.TRANSPARENT);
+        gradientDrawable.setStroke(SDViewUtil.dp2px(1), mTextColorEnable);
+        gradientDrawable.setCornerRadius(SDViewUtil.dp2px(5));
 
-		SDViewUtil.setBackgroundDrawable(this, gradientDrawable);
+        SDViewUtil.setBackgroundDrawable(this, gradientDrawable);
 
-		updateViewState(true);
-		this.setOnClickListener(new OnClickListener()
-		{
+        updateViewState(true);
+        this.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v)
-			{
-				if (mListener != null && mClickAble)
-				{
-					mListener.onClickSendValidateButton();
-				}
-			}
-		});
-	}
+            @Override
+            public void onClick(View v) {
+                if (mListener != null && mClickAble) {
+                    mListener.onClickSendValidateButton();
+                }
+            }
+        });
+    }
 
-	public void startTickWork()
-	{
-		stopTickWork();
-		if (mClickAble)
-		{
-			mClickAble = false;
-			updateViewState(false);
-			mDisableTimeTemp = mDisableTime;
-			mTimer.startWork(0, 1000, new SDTimerListener()
-			{
+    public void startTickWork() {
+        stopTickWork();
+        if (mClickAble) {
+            mClickAble = false;
+            updateViewState(false);
+            mDisableTimeTemp = mDisableTime;
+            mTimer.startWork(0, 1000, new SDTimerListener() {
 
-				@Override
-				public void onWorkMain()
-				{
-					tickWork();
-				}
+                @Override
+                public void onWorkMain() {
+                    tickWork();
+                }
 
-				@Override
-				public void onWork()
-				{
-				}
-			});
-		}
-	}
+                @Override
+                public void onWork() {
+                }
+            });
+        }
+    }
 
-	public void updateViewState(boolean enable)
-	{
-		if (enable)
-		{
-			this.setText(mTextEnable);
-			if (mTextColorEnable != 0)
-			{
-				this.setTextColor(mTextColorEnable);
-			}
-			if (mBackgroundEnableResId != 0)
-			{
-				SDViewUtil.setBackgroundResource(this, mBackgroundEnableResId);
-			}
-		} else
-		{
+    public void updateViewState(boolean enable) {
+        if (enable) {
+            this.setText(mTextEnable);
+            if (mTextColorEnable != 0) {
+                this.setTextColor(mTextColorEnable);
+            }
+            if (mBackgroundEnableResId != 0) {
+                SDViewUtil.setBackgroundResource(this, mBackgroundEnableResId);
+            }
+        } else {
 //			this.setText(mDisableTimeTemp + mTextDisable);
-			this.setText(mTextDisable + "(" + mDisableTimeTemp + ")");
-			if (mTextColorDisable != 0)
-			{
-				this.setTextColor(mTextColorDisable);
-			}
-			if (mBackgroundDisableResId != 0)
-			{
-				SDViewUtil.setBackgroundResource(this, mBackgroundDisableResId);
-			}
-		}
-	}
+            this.setText(mTextDisable + "(" + mDisableTimeTemp + ")");
+            if (mTextColorDisable != 0) {
+                this.setTextColor(mTextColorDisable);
+            }
+            if (mBackgroundDisableResId != 0) {
+                SDViewUtil.setBackgroundResource(this, mBackgroundDisableResId);
+            }
+        }
+    }
 
-	/**
-	 * 每秒钟调用一次
-	 */
-	private void tickWork()
-	{
-		mDisableTimeTemp--;
+    /**
+     * 每秒钟调用一次
+     */
+    private void tickWork() {
+        mDisableTimeTemp--;
 //		this.setText(mDisableTimeTemp + mTextDisable);
-		this.setText(mTextDisable + "(" + mDisableTimeTemp + ")");
-		if (mListener != null)
-		{
-			mListener.onTick();
-		}
-		if (mDisableTimeTemp < 0)
-		{
-			stopTickWork();
-		}
-	}
+        this.setText(mTextDisable + "(" + mDisableTimeTemp + ")");
+        if (mListener != null) {
+            mListener.onTick();
+        }
+        if (mDisableTimeTemp < 0) {
+            stopTickWork();
+        }
+    }
 
-	public void stopTickWork()
-	{
-		mTimer.stopWork();
-		mDisableTimeTemp = mDisableTime;
-		updateViewState(true);
-		mClickAble = true;
-	}
+    public void stopTickWork() {
+        mTimer.stopWork();
+        mDisableTimeTemp = mDisableTime;
+        updateViewState(true);
+        mClickAble = true;
+    }
 
-	public interface SDSendValidateButtonListener
-	{
-		public void onClickSendValidateButton();
+    public interface SDSendValidateButtonListener {
+        public void onClickSendValidateButton();
 
-		public void onTick();
-	}
+        public void onTick();
+    }
 
 }
